8829a014a923ccaf670d7c8637e29475de7563a9
rinomakin <rinomakin@gmail.com>
2025-08-07 22:14:04 +0800

    penambahan fitur

--- resources/views/admin/anggota/cetak-kartu.blade.php
+++ resources/views/admin/anggota/cetak-kartu.blade.php
@@ -28,9 +28,9 @@
         <i class="fas fa-print mr-2"></i>Cetak Kartu
     </button>
     
-    <div class="w-[700px] h-[250px] mx-auto bg-white shadow-lg rounded-lg flex overflow-hidden border">
+    <div class="w-[700px] h-[250px] mx-auto bg-inherit bg-gradient-to-r from-blue-500 to-indigo-600 shadow-lg rounded-lg flex overflow-hidden border">
         <!-- Left Panel - Member Information -->
-        <div class="w-[350px] h-[250px] p-4 bg-white border-r-2 border-gray-200">
+        <div class="w-[350px] h-[250px] p-4  border-r-2 border-gray-200">
             <div class="flex">
                <div class="w-1/2">
                 <div class="flex items-center justify-center mb-2">
@@ -38,30 +38,30 @@
                     <img src="{{ asset($pengaturan->logo) }}" alt="Logo" class="h-10 w-auto">
                 </div>
                     <div class="text-center">
-                        <div class="text-[8px] font-bold text-gray-800 uppercase">KARTU PERPUSTAKAAN</div>
-                        <div class="text-[8px] font-bold text-gray-800 uppercase">SMA Negeri 2 kefamenanu</div>
-                        <div class="text-[8px] text-gray-600 mb-4">Jl. Contoh Alamat No. 123, Kota, Provinsi</div>
-                        <div class="text-[14px] font-bold text-gray-800 uppercase  ">
+                        <div class="text-[8px] font-bold text-white uppercase">KARTU PERPUSTAKAAN</div>
+                        <div class="text-[8px] font-bold text-white uppercase">SMA Negeri 2 kefamenanu</div>
+                        <div class="text-[8px] text-white mb-5">Jl. Contoh Alamat No. 123, Kota, Provinsi</div>
+                        <div class="text-[10px] font-bold mb-7 text-white uppercase border-b-2 border-gray-400 pb-1">
                                      {{ $anggota->nama_lengkap }}
                         </div>
                         <div class="inline-block p-2 mb-4  rounded-lg max-w-[150px]">
                     <img src="data:image/png;base64,{{ \App\Helpers\BarcodeHelper::generateBarcodeImage($anggota->barcode_anggota, 'C128') }}" 
-                         alt="Barcode" class="barcode-image mb-1">
-                    <div class="barcode-text text-[10px] text-gray-700">{{ $anggota->barcode_anggota }}</div>
+                         alt="Barcode" class="barcode-image text-white mb-1">
+                    <div class="barcode-text text-[10px] text-white">{{ $anggota->barcode_anggota }}</div>
                 </div>
                 </div>
             </div>
             <div class="flex-grow text-right ">
                 <div class="text-[14px] font-bold bg-slate-500 text-center text-white mb-2 ">{{ strtoupper($anggota->jenis_anggota) }}</div>
-                <div class="text-[10px] text-gray-600 font-bold text-center mb-2">{{ $anggota->nomor_anggota }}</div>
-                <div class="text-[8px] text-gray-600 font-semibold text-center">Kartu Berlaku Hingga </div>
-                <div class="text-[8px] text-gray-600 font-bold text-center mb-2"> Selama Menjadi Siswa</div>
+                <div class="text-[10px] text-white font-bold text-center mb-2">{{ $anggota->nomor_anggota }}</div>
+                <div class="text-[8px] text-white font-semibold text-center">Kartu Berlaku Hingga </div>
+                <div class="text-[8px] text-white font-bold text-center mb-2"> Selama Menjadi Siswa</div>
                 <div class="w-[90px] h-[110px] bg-gray-200 mb-2 border-gray-300 rounded flex items-center justify-center mx-auto">
                     @if($anggota->foto)
                         <img src="{{ asset('storage/anggota/' . $anggota->foto) }}" 
                              alt="Foto" class="w-full h-full object-cover rounded">
                     @else
-                        <i class="fas fa-user text-gray-500 text-lg"></i>
+                        <i class="fas fa-user text-white text-lg"></i>
                     @endif
                 </div>
                 
@@ -73,15 +73,15 @@
         </div>
         
         <!-- Right Panel - Library Rules -->
-        <div class="w-[350px] h-[250px] p-4 bg-white">
+        <div class="w-[350px] h-[250px] p-4 ">
             <div class="text-center mb-4">
                 <div class="text-sm font-bold text-black uppercase border-b-2 border-black-200 pb-2">
                     PERATURAN PERPUSTAKAAN
                 </div>
             </div>
             
-            <div class="text-xs text-gray-700 leading-tight mb-4">
-                <ol class="list-decimal text-[8px] font-semibold list-inside space-y-1">
+            <div class="text-xs text-white leading-tight mb-4">
+                <ol class="list-decimal text-[8px] font-semibold text-white list-inside space-y-1">
                     <li>Kartu dibawa setiap berkunjung ke Perpustakaan.</li>
                     <li>Kartu tidak dapat dipinjamkan kepada orang lain.</li>
                     <li>Kartu berlaku selama menjadi anggota perpustakaan.</li>
@@ -91,7 +91,7 @@
                     <li>Kartu ini dapat dicabut apabila yang bersangkutan tidak memenuhi ketentuan diatas</li>
                 </ol>
             </div>
-            <div class="text-xs text-gray-600 text-right pr-5">
+            <div class="text-xs text-white text-right pr-5">
                 <div class=" text-[8px]">Kefamenanu, {{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}</div>
                 <div class="font-bold text-[8px]">Kepala Perpustakaan</div>
                 <div class="mt-3 ">
